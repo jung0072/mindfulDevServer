0a2929f35b27c8abb66f90b2e3e635e9531d9230
jung0072 <jung0072@algonquinlive.com>
2023-06-16 21:07:23 -0400

    update env variables

--- mindfuldev/views.py
+++ mindfuldev/views.py
@@ -12,7 +12,7 @@ from datetime import datetime
 load_dotenv()
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 class MeditationScriptView(APIView):
@@ -43,7 +43,7 @@ class MeditationScriptView(APIView):
         print("\033[92m" + "Script saved to file" + "\033[0m")
         script = texttospeech.SynthesisInput(ssml=script)
         client = texttospeech.TextToSpeechClient(
-            client_options={"api_key": os.getenv("GOOGLE_API_KEY")}
+            client_options={"api_key": os.environ.get("GOOGLE_API_KEY")}
         )
 
         voice = texttospeech.VoiceSelectionParams(
